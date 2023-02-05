b50e2998796135a5936eb569d518373a7ca436c5
jaypark7821 <pjhyun1990@gmail.com>
2023-02-05 22:35:07 +0900

    test UserFacade::getLoginInforFrom 실패하는 테스트 작성

---
+++ src/test/java/kr/jay/okrver3/application/user/UserFacadeTest.java
@@ -0,0 +1,42 @@
+package kr.jay.okrver3.application.user;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import kr.jay.okrver3.domain.user.ProviderType;
+import kr.jay.okrver3.interfaces.user.OAuth2UserInfo;
+
+class UserFacadeTest {
+
+	private UserFacade sut;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		sut = new UserFacade();
+	}
+
+	@Test
+	@DisplayName("가입한 유저 정보가 없는 OAuth2UserInfo가 넘어왔을 때 기대하는 응답(Guest)을 반환한다.")
+	void login_With_IdToken_when_before_join() throws Exception {
+
+		LoginInfo loginInfo = sut.getLoginInfoFrom(
+			new OAuth2UserInfo("googleId", "userName", "email", "pictureUrl", ProviderType.GOOGLE));
+
+		Assertions.assertThat(loginInfo).isEqualTo(
+			new LoginInfo(
+				"guest-12301",
+				"name",
+				"email",
+				ProviderType.GOOGLE,
+				null,
+				null
+			)
+		);
+
+	}
+
+}
