4cfebfca1f00b7587aa174bebca1c2dff69dde48
JayPark7821 <pjhyun1990@gmail.com>
2023-02-06 18:44:35 +0900

    test UserApiController::loginWithIdToken 테스트 케이스 추가

--- src/main/java/kr/jay/okrver3/domain/user/ProviderType.java
+++ src/main/java/kr/jay/okrver3/domain/user/ProviderType.java
@@ -1,5 +1,12 @@
 package kr.jay.okrver3.domain.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum ProviderType {
-	GOOGLE
+	APPLE("Apple"), GOOGLE("Google");
+
+	private String name;
 }
--- src/test/java/kr/jay/okrver3/interfaces/user/UserApiControllerTest.java
+++ src/test/java/kr/jay/okrver3/interfaces/user/UserApiControllerTest.java
@@ -1,10 +1,10 @@
 package kr.jay.okrver3.interfaces.user;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
 import java.util.Optional;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -31,21 +31,22 @@ class UserApiControllerTest {
 	@BeforeEach
 	void setUp() throws Exception {
 		sut = new UserApiController(tokenVerifier,userFacade);
-		given(tokenVerifier.verifyIdToken("idToken")).willReturn(new OAuth2UserInfo("googleId", "userName", "email","pictureUrl",ProviderType.GOOGLE));
-		given(userFacade.getLoginInfoFrom(new OAuth2UserInfo("googleId", "userName", "email","pictureUrl",ProviderType.GOOGLE)))
-			.willReturn(Optional.empty());
-		given(userFacade.createGuestInfoFrom(new OAuth2UserInfo("googleId", "userName", "email","pictureUrl",ProviderType.GOOGLE)))
-			.willReturn(new LoginInfo("guest-12301", "name", "email", ProviderType.GOOGLE, "pic",null, null));
 
 	}
 //
 	@Test
 	@DisplayName("가입한 유저 정보가 없을 때  idToken을 통해 로그인을 시도하면 기대하는 응답(Guest)을 반환한다.")
 	void login_With_IdToken_when_before_join() throws Exception {
+		given(tokenVerifier.verifyIdToken("idToken")).willReturn(new OAuth2UserInfo("googleId", "userName", "email","pictureUrl",ProviderType.GOOGLE));
+		given(userFacade.getLoginInfoFrom(new OAuth2UserInfo("googleId", "userName", "email","pictureUrl",ProviderType.GOOGLE)))
+			.willReturn(Optional.empty());
+		given(userFacade.createGuestInfoFrom(new OAuth2UserInfo("googleId", "userName", "email","pictureUrl",ProviderType.GOOGLE)))
+			.willReturn(new LoginInfo("guest-12301", "name", "email", ProviderType.GOOGLE, "pic",null, null));
+
 
 		final ResponseEntity<LoginResponse> response = sut.loginWithIdToken("GOOGLE", "idToken");
 
-		Assertions.assertThat(response.getBody()).isEqualTo(
+		assertThat(response.getBody()).isEqualTo(
 			new LoginResponse(
 				"guest-12301",
 				"name",
@@ -56,4 +57,21 @@ class UserApiControllerTest {
 			)
 		);
 	}
+
+	@Test
+	@DisplayName("가입한 유저 정보가 있지만 가입한 소셜 정보와 다른 소셜 idToken을 통해 로그인을 시도하면 기대하는 응답(Exception)을 반환한다.")
+	void login_With_IdToken_when_after_join() throws Exception {
+
+		OAuth2UserInfo info = new OAuth2UserInfo("googleId", "userName", "email", "pictureUrl", ProviderType.GOOGLE);
+
+		given(tokenVerifier.verifyIdToken("idToken"))
+			.willReturn(info);
+		given(userFacade.getLoginInfoFrom(info))
+			.willThrow(new IllegalArgumentException(ProviderType.APPLE.getName() + "(으)로 가입한 계정이 있습니다."));
+
+		assertThatThrownBy(() -> sut.loginWithIdToken("GOOGLE", "idToken"))
+			.isExactlyInstanceOf(IllegalArgumentException.class)
+			.hasMessage(ProviderType.APPLE.getName() + "(으)로 가입한 계정이 있습니다.");
+	}
+
 }
