1dc3c61d980d082af482bfac195b31dd729c267c
JayPark7821 <pjhyun1990@gmail.com>
2023-02-07 15:41:53 +0900

    test UserServiceImpl::getUserInfoFrom 테스트 케이스 추가.

--- src/test/java/kr/jay/okrver3/domain/user/service/impl/UserServiceImplTest.java
+++ src/test/java/kr/jay/okrver3/domain/user/service/impl/UserServiceImplTest.java
@@ -2,6 +2,8 @@ package kr.jay.okrver3.domain.user.service.impl;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +12,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.jdbc.Sql;
 
 import kr.jay.okrver3.domain.user.ProviderType;
-import kr.jay.okrver3.domain.user.service.UserReader;
+import kr.jay.okrver3.domain.user.UserInfo;
 import kr.jay.okrver3.infrastructure.user.UserReaderImpl;
 import kr.jay.okrver3.interfaces.user.OAuth2UserInfo;
 
@@ -21,8 +23,6 @@ class UserServiceImplTest{
 	@Autowired
 	private UserServiceImpl sut;
 
-	@Autowired
-	private UserReader userReader;
 
 	@Test
 	@Sql("classpath:insert-user.sql")
@@ -34,6 +34,34 @@ class UserServiceImplTest{
 		assertThatThrownBy(() -> sut.getUserInfoFrom(info))
 			.isExactlyInstanceOf(IllegalArgumentException.class)
 			.hasMessage(ProviderType.APPLE.getName() + "(으)로 가입한 계정이 있습니다.");
+	}
 
+	@Test
+	@DisplayName("가입한 유저 정보가 없을때 idToken을 통해 로그인을 시도하면 기대하는 응답(Optional.empty())을 반환한다.")
+	void try_to_login_with_social_IdToken_for_the_first_time() throws Exception {
+
+		OAuth2UserInfo info =
+			new OAuth2UserInfo("googleId", "userName", "apple@apple.com", "pictureUrl", ProviderType.GOOGLE);
+
+		Optional<UserInfo> userInfoFrom = sut.getUserInfoFrom(info);
+
+		assertThat(userInfoFrom).isEmpty();
+	}
+
+	@Test
+	@Sql("classpath:insert-user.sql")
+	@DisplayName("가입한 유저 정보가 있을때 idToken을 통해 로그인을 시도하면 기대하는 응답(UserInfo)을 반환한다.")
+	void will_return_userInfo_when_user_try_to_login_with_idToken() throws Exception {
+		OAuth2UserInfo info =
+			new OAuth2UserInfo("appleId", "appleUser", "apple@apple.com", "appleProfileImage", ProviderType.APPLE);
+
+		Optional<UserInfo> userInfoFrom = sut.getUserInfoFrom(info);
+
+		assertThat(userInfoFrom).isNotEmpty();
+		assertThat(userInfoFrom.get().email()).isEqualTo(info.email());
+		assertThat(userInfoFrom.get().id()).isEqualTo(info.id());
+		assertThat(userInfoFrom.get().name()).isEqualTo(info.name());
+		assertThat(userInfoFrom.get().profileImageUrl()).isEqualTo(info.picture());
+		assertThat(userInfoFrom.get().providerType()).isEqualTo(info.providerType());
 	}
 }
