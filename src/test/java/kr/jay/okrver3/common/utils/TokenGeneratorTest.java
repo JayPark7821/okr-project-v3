a69cf756880a517b7a1a73eafdb92101ace7fdf2
JayPark7821 <pjhyun1990@gmail.com>
2023-02-07 16:40:11 +0900

    feat TokenGenerator 테스트 추가 & 구현

---
+++ src/test/java/kr/jay/okrver3/common/utils/TokenGeneratorTest.java
@@ -0,0 +1,16 @@
+package kr.jay.okrver3.common.utils;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.regex.Pattern;
+
+import org.junit.jupiter.api.Test;
+
+class TokenGeneratorTest {
+
+	@Test
+	void randomCharacterWithPrefixTest() {
+		assertThat(TokenGenerator.randomCharacterWithPrefix("prefix-")).containsPattern(
+			Pattern.compile("prefix-[a-zA-Z0-9]{13}"));
+	}
+}
