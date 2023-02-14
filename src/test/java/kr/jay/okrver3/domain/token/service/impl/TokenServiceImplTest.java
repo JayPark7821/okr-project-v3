ecc5459a06405a559b16804aab8daaf012996bf5
JayPark7821 <pjhyun1990@gmail.com>
2023-02-14 12:34:15 +0900

    test TokenServiceImpl::generateTokenSet 실패하는 테스트 케이스 추가.

--- src/test/java/kr/jay/okrver3/domain/token/service/impl/TokenServiceImplTest.java
+++ src/test/java/kr/jay/okrver3/domain/token/service/impl/TokenServiceImplTest.java
@@ -7,8 +7,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.jdbc.Sql;
 
+import kr.jay.okrver3.common.utils.JwtTokenUtils;
+import kr.jay.okrver3.domain.token.RefreshToken;
+import kr.jay.okrver3.domain.token.RefreshTokenRepository;
 import kr.jay.okrver3.domain.token.service.AuthTokenInfo;
 import kr.jay.okrver3.domain.user.ProviderType;
 import kr.jay.okrver3.domain.user.service.UserInfo;
@@ -20,6 +22,10 @@ class TokenServiceImplTest {
 	@Autowired
 	private TokenServiceImpl sut;
 
+	@Autowired
+	private RefreshTokenRepository refreshTokenRepository;
+
+	private static final String TOKEN_KEY = "secretKey-test-okr-project-jwt-token";
 	@Test
 	@DisplayName("email이 입력 되었을때 기대하는 응답(AuthTokenInfo)을 반환한다.")
 	void create_new_authTokenInfo () throws Exception {
@@ -33,30 +39,37 @@ class TokenServiceImplTest {
 	}
 
 	@Test
-	@Sql("classpath:insert-refresh-token.sql")
 	@DisplayName("남은 기간이 3일 이상인 refreshToken이 있을때 기존 refreshToken을 반환한다.")
 	void returns_old_refreshToken_when_expire_date_more_then_3days () throws Exception {
+		//given
 		UserInfo userInfo =
 			new UserInfo(1L, "appleId", "appleUser", "apple@apple.com", "appleProfileImage", ProviderType.APPLE);
+		String UsableRefreshToken = JwtTokenUtils.generateToken(userInfo.email(), TOKEN_KEY, 259300000L);
+		refreshTokenRepository.save(new RefreshToken(1L,UsableRefreshToken ));
 
-
+		//when
 		AuthTokenInfo authTokenInfo = sut.generateTokenSet(userInfo);
 
+		//then
 		assertThat(authTokenInfo.accessToken()).isNotNull();
-		assertThat(authTokenInfo.refreshToken()).isNotNull();
+		assertThat(authTokenInfo.refreshToken()).isEqualTo(UsableRefreshToken);
 	}
 
 	@Test
-	@Sql("classpath:insert-refresh-token.sql")
 	@DisplayName("남은 기간이 3일 미만인 refreshToken이 있을때 새로운 refreshToken을 반환한다.")
 	void returns_new_refreshToken_when_expire_date_less_then_3days () throws Exception {
+		//given
 		UserInfo userInfo =
 			new UserInfo(2L, "googleId", "googleUser", "google@google.com", "googleProfileImage", ProviderType.GOOGLE);
+		String UsableRefreshToken = JwtTokenUtils.generateToken(userInfo.email(), TOKEN_KEY, 1000L);
+		refreshTokenRepository.save(new RefreshToken(2L,UsableRefreshToken ));
 
+		//when
 		AuthTokenInfo authTokenInfo = sut.generateTokenSet(userInfo);
 
+		//then
 		assertThat(authTokenInfo.accessToken()).isNotNull();
-		assertThat(authTokenInfo.refreshToken()).isNotNull();
+		assertThat(authTokenInfo.refreshToken()).isNotEqualTo(UsableRefreshToken);
 	}
 
 }
