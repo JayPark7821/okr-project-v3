785f1dc4955b424aa314fbd75050a572e12af53c
jaypark7821 <pjhyun1990@gmail.com>
2023-02-15 15:01:50 +0900

    test ProjectApiController::registerProject 실패하는 테스트 작성

---
+++ src/test/java/kr/jay/okrver3/interfaces/project/ProjectApiControllerTest.java
@@ -0,0 +1,49 @@
+package kr.jay.okrver3.interfaces.project;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.test.context.jdbc.Sql;
+
+import kr.jay.okrver3.domain.user.ProviderType;
+import kr.jay.okrver3.domain.user.RoleType;
+import kr.jay.okrver3.domain.user.User;
+
+@Sql("classpath:insert-user.sql")
+@SpringBootTest
+class ProjectApiControllerTest {
+
+	@Autowired
+	private ProjectApiController sut;
+
+	@Test
+	@DisplayName("프로젝트를 생성하면 기대하는 응답(projectToken)을 반환한다.")
+	void create_project() throws Exception {
+
+		User user = new User(1L, "appleId", "appleUser", "apple@apple.com", "appleProfileImage", ProviderType.APPLE,
+			RoleType.ADMIN, "pass");
+		String projectSdt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+		String projectEdt = LocalDateTime.now().plusDays(10).format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+
+		UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+			user, null, user.getAuthorities());
+
+		final ResponseEntity<String> response = sut.registerProject(
+			new ProjectMasterSaveDto("projectName", projectSdt, projectEdt, "projectObjective",
+				List.of("keyResult1", "keyResult2")), auth);
+
+		assertThat(response.getBody()).containsPattern(
+			Pattern.compile("project-[a-zA-Z0-9]{12}"));
+	}
+
+}
