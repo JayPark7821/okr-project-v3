97c344312435d894367ea23e8a109024e0debb26
jaypark7821 <pjhyun1990@gmail.com>
2023-02-15 22:48:17 +0900

    feat TeamMemberApiController::inviteTeamMember 구현

---
+++ src/main/java/kr/jay/okrver3/application/team/TeamMemberFacade.java
@@ -0,0 +1,10 @@
+package kr.jay.okrver3.application.team;
+
+import kr.jay.okrver3.domain.user.User;
+import kr.jay.okrver3.interfaces.team.TeamMemberInviteRequestDto;
+
+public class TeamMemberFacade {
+	public String inviteTeamMember(TeamMemberInviteRequestDto requestDto, User user) {
+		throw new UnsupportedOperationException("kr.jay.okrver3.application.team.TeamMemberFacade.inviteTeamMember()");
+	}
+}
--- src/main/java/kr/jay/okrver3/interfaces/team/TeamMemberApiController.java
+++ src/main/java/kr/jay/okrver3/interfaces/team/TeamMemberApiController.java
@@ -2,6 +2,7 @@ package kr.jay.okrver3.interfaces.team;
 
 import javax.validation.Valid;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.jay.okrver3.application.team.TeamMemberFacade;
+import kr.jay.okrver3.domain.user.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,11 +21,15 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/api/v1/team")
 public class TeamMemberApiController {
 
+	private final TeamMemberFacade teamMemberFacade;
+
 	@PostMapping("/invite")
 	ResponseEntity<String> inviteTeamMember(
 		@RequestBody @Valid TeamMemberInviteRequestDto requestDto,
 		Authentication authentication
 	) {
-		throw new UnsupportedOperationException("Not yet implemented");
+		
+		return ResponseEntity.status(HttpStatus.CREATED)
+			.body(teamMemberFacade.inviteTeamMember(requestDto, (User)authentication.getPrincipal()));
 	}
 }
