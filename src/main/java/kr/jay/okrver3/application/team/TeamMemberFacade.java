955c96b7d2ce1a553d24296bb52bc51abdd3af2a
jaypark7821 <pjhyun1990@gmail.com>
2023-02-15 22:53:43 +0900

    test TeamMemberFacade::inviteTeamMember 실패하는 테스트 추가.

--- src/main/java/kr/jay/okrver3/application/team/TeamMemberFacade.java
+++ src/main/java/kr/jay/okrver3/application/team/TeamMemberFacade.java
@@ -1,9 +1,17 @@
 package kr.jay.okrver3.application.team;
 
+import org.springframework.stereotype.Service;
+
 import kr.jay.okrver3.domain.user.User;
 import kr.jay.okrver3.interfaces.team.TeamMemberInviteRequestDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class TeamMemberFacade {
+
 	public String inviteTeamMember(TeamMemberInviteRequestDto requestDto, User user) {
 		throw new UnsupportedOperationException("kr.jay.okrver3.application.team.TeamMemberFacade.inviteTeamMember()");
 	}
