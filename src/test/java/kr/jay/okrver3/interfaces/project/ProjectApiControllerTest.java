abe467e377192f43a4c7e61a736a8ef6f558e886
JayPark7821 <pjhyun1990@gmail.com>
2023-02-28 14:05:10 +0900

    refactor ProjectApiControllerTest 엔티티 조회 변경 Q <-----

--- src/test/java/kr/jay/okrver3/interfaces/project/ProjectApiControllerTest.java
+++ src/test/java/kr/jay/okrver3/interfaces/project/ProjectApiControllerTest.java
@@ -7,6 +7,9 @@ import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.regex.Pattern;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,9 +20,6 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 
 import kr.jay.okrver3.common.exception.ErrorCode;
-import kr.jay.okrver3.domain.user.JobFieldDetail;
-import kr.jay.okrver3.domain.user.ProviderType;
-import kr.jay.okrver3.domain.user.RoleType;
 import kr.jay.okrver3.domain.user.User;
 
 @Transactional
@@ -29,12 +29,18 @@ class ProjectApiControllerTest {
 	@Autowired
 	private ProjectApiController sut;
 
+	@PersistenceContext
+	EntityManager em;
+
 	@Test
+	@Sql("classpath:insert-user.sql")
 	@DisplayName("프로젝트를 생성하면 기대하는 응답(projectToken)을 반환한다.")
 	void create_project() throws Exception {
 
-		User user = new User(1L, "appleId", "appleUser", "apple@apple.com", "appleProfileImage", ProviderType.APPLE,
-			RoleType.ADMIN, "pass", JobFieldDetail.WEB_FRONT_END_DEVELOPER);
+		User user = em.createQuery("select u from User u where u.id = :userSeq", User.class)
+			.setParameter("userSeq", 1L)
+			.getSingleResult();
+
 		String projectSdt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
 		String projectEdt = LocalDateTime.now().plusDays(10).format(DateTimeFormatter.ofPattern("yyyyMMdd"));
 
@@ -50,11 +56,14 @@ class ProjectApiControllerTest {
 	}
 
 	@Test
+	@Sql("classpath:insert-user.sql")
 	@DisplayName("프로젝트를 생성시 팀원을 같이 입력하면 기대하는 응답(projectToken)을 반환한다.")
 	void create_project_with_team_members() throws Exception {
 
-		User user = new User(1L, "appleId", "appleUser", "apple@apple.com", "appleProfileImage", ProviderType.APPLE,
-			RoleType.ADMIN, "pass", JobFieldDetail.WEB_FRONT_END_DEVELOPER);
+		User user = em.createQuery("select u from User u where u.id = :userSeq", User.class)
+			.setParameter("userSeq", 1L)
+			.getSingleResult();
+
 		String projectSdt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
 		String projectEdt = LocalDateTime.now().plusDays(10).format(DateTimeFormatter.ofPattern("yyyyMMdd"));
 
@@ -74,8 +83,9 @@ class ProjectApiControllerTest {
 	@DisplayName("projectToken으로 조회하면 기대하는 응답(ProjectResponse)을 반환한다.")
 	void retrieve_project_with_project_token() throws Exception {
 
-		User user = new User(1L, "appleId", "appleUser", "apple@apple.com", "appleProfileImage", ProviderType.APPLE,
-			RoleType.ADMIN, "pass", JobFieldDetail.WEB_FRONT_END_DEVELOPER);
+		User user = em.createQuery("select u from User u where u.id = :userSeq", User.class)
+			.setParameter("userSeq", 1L)
+			.getSingleResult();
 
 		UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
 			user, null, user.getAuthorities());
@@ -95,8 +105,9 @@ class ProjectApiControllerTest {
 	@DisplayName("팀원 추가를 시도하면 기대하는 응답(추가된 email주소)을 반환한다.")
 	void invite_team_member() throws Exception {
 
-		User user = new User(1L, "appleId", "appleUser", "apple@apple.com", "appleProfileImage", ProviderType.APPLE,
-			RoleType.ADMIN, "pass", JobFieldDetail.WEB_FRONT_END_DEVELOPER);
+		User user = em.createQuery("select u from User u where u.id = :userSeq", User.class)
+			.setParameter("userSeq", 1L)
+			.getSingleResult();
 
 		UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
 			user, null, user.getAuthorities());
@@ -109,12 +120,14 @@ class ProjectApiControllerTest {
 
 
 	@Test
+	@Sql("classpath:insert-user.sql")
 	@DisplayName("팀원 추가를 위해 email을 입력하면 기대하는 응답(email)을 반환한다.")
 	void validate_email_address() throws Exception {
 
 		String memberEmail = "guest@email.com";
-		User user = new User(1L, "appleId", "appleUser", "apple@apple.com", "appleProfileImage", ProviderType.APPLE,
-			RoleType.ADMIN, "pass", JobFieldDetail.WEB_FRONT_END_DEVELOPER);
+		User user = em.createQuery("select u from User u where u.id = :userSeq", User.class)
+			.setParameter("userSeq", 1L)
+			.getSingleResult();
 
 
 		UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
@@ -127,12 +140,14 @@ class ProjectApiControllerTest {
 
 
 	@Test
+	@Sql("classpath:insert-user.sql")
 	@DisplayName("로그인한 유저가 속하지 않은 프로젝트에 팀원 추가를 위해 email을 입력하면 기대하는 응답(exception)을 반환한다.")
 	void validate_email_address_with_not_participating_project_throw_exception() throws Exception {
 
 		String memberEmail = "guest@email.com";
-		User user = new User(2L, "fakeAppleId", "fakeAppleName", "fakeAppleEmail", "fakeApplePic", ProviderType.APPLE,
-			RoleType.ADMIN, "pass", JobFieldDetail.WEB_FRONT_END_DEVELOPER);
+		User user = em.createQuery("select u from User u where u.id = :userSeq", User.class)
+			.setParameter("userSeq", 2L)
+			.getSingleResult();
 
 
 		UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
@@ -146,11 +161,13 @@ class ProjectApiControllerTest {
 
 
 	@Test
+	@Sql("classpath:insert-user.sql")
 	@DisplayName("리더가 아닌 팀원이 팀원 추가를 위해 email을 입력하면 기대하는 응답(exception)을 반환한다.")
 	void when_member_validate_email_address_will_throw_exception() throws Exception {
 		String memberEmail = "guest@email.com";
-		User user = new User(3L, "fakeGoogleId", "fakeGoogleName", "fakeGoogleIdEmail", "fakeGoogleIdPic", ProviderType.GOOGLE,
-			RoleType.ADMIN, "pass", JobFieldDetail.WEB_SERVER_DEVELOPER);
+		User user = em.createQuery("select u from User u where u.id = :userSeq", User.class)
+			.setParameter("userSeq", 3L)
+			.getSingleResult();
 
 
 		UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
@@ -163,13 +180,14 @@ class ProjectApiControllerTest {
 	}
 
 	@Test
+	@Sql("classpath:insert-user.sql")
 	@DisplayName("팀원 추가를 위해 잘못된 email을 입력하면 기대하는 응답(exception)을 반환한다.")
 	void validate_email_address_exception() throws Exception {
 
 		String wrongEmailAdd = "wrongEmailAdd";
-		User user = new User(1L, "appleId", "appleUser", "apple@apple.com", "appleProfileImage", ProviderType.APPLE,
-			RoleType.ADMIN, "pass", JobFieldDetail.WEB_FRONT_END_DEVELOPER);
-
+		User user = em.createQuery("select u from User u where u.id = :userSeq", User.class)
+			.setParameter("userSeq", 1L)
+			.getSingleResult();
 
 		UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
 			user, null, user.getAuthorities());
@@ -180,12 +198,13 @@ class ProjectApiControllerTest {
 	}
 
 	@Test
+	@Sql("classpath:insert-user.sql")
 	@DisplayName("이미 팀에 초대된 팀원의 email을 입력하면 기대하는 응답(exception)을 반환한다.")
 	void validate_email_address_already_team_member() throws Exception {
 		String teamMemberEmail = "fakeGoogleIdEmail";
-		User user = new User(1L, "appleId", "appleUser", "apple@apple.com", "appleProfileImage", ProviderType.APPLE,
-			RoleType.ADMIN, "pass", JobFieldDetail.WEB_FRONT_END_DEVELOPER);
-
+		User user = em.createQuery("select u from User u where u.id = :userSeq", User.class)
+			.setParameter("userSeq", 1L)
+			.getSingleResult();
 
 		UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
 			user, null, user.getAuthorities());
@@ -196,11 +215,13 @@ class ProjectApiControllerTest {
 	}
 
 	@Test
+	@Sql("classpath:insert-user.sql")
 	@DisplayName("로그인된 유저 자신의 email을 입력하면 기대하는 응답(exception)을 반환한다.")
 	void validate_email_address_login_user_email() throws Exception {
 		String userEmail = "'apple@apple.com'";
-		User user = new User(1L, "appleId", "appleUser", "apple@apple.com", "appleProfileImage", ProviderType.APPLE,
-			RoleType.ADMIN, "pass", JobFieldDetail.WEB_FRONT_END_DEVELOPER);
+		User user = em.createQuery("select u from User u where u.id = :userSeq", User.class)
+			.setParameter("userSeq", 1L)
+			.getSingleResult();
 
 		UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
 			user, null, user.getAuthorities());
