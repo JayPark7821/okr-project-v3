82f1a05cb3b9cbb73699069f752a3753f770a0fc
jaypark7821 <pjhyun1990@gmail.com>
2023-03-05 00:32:23 +0900

    test ProjectApiController::initiativeFinished 실패하는 테스트 추가

--- src/main/java/kr/jay/okrver3/interfaces/project/ProjectApiController.java
+++ src/main/java/kr/jay/okrver3/interfaces/project/ProjectApiController.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -173,5 +174,13 @@ public class ProjectApiController {
 				HttpStatus.CREATED,
 				projectFacade.registerInitiative(requestDto.toCommand(), user)
 			);
+	}
+
+	@PutMapping("/initiative/{initiativeToken}/done")
+	public ResponseEntity<String> initiativeFinished(
+		@PathVariable("initiativeToken") String initiativeToken,
+		Authentication authentication
+	) {
+		return null;
 	}
 }
