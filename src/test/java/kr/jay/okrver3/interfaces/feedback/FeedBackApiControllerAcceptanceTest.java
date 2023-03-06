bcb6e45d9f279c2c6b3fe714b4715fa4b615a49f
JayPark7821 <pjhyun1990@gmail.com>
2023-03-06 16:33:13 +0900

    test 행동전략에 피드백 작성 인수테스트 추가

---
+++ src/test/java/kr/jay/okrver3/interfaces/feedback/FeedBackApiControllerAcceptanceTest.java
@@ -0,0 +1,95 @@
+package kr.jay.okrver3.interfaces.feedback;
+
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import java.sql.Connection;
+import java.util.List;
+import java.util.regex.Pattern;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.transaction.annotation.Transactional;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import kr.jay.okrver3.common.utils.JwtTokenUtils;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@Transactional
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class FeedBackApiControllerAcceptanceTest {
+
+	@Value("${app.auth.tokenSecret}")
+	private String key;
+
+	@Value("${app.auth.tokenExpiry}")
+	private Long accessExpiredTimeMs;
+
+	@PersistenceContext
+	EntityManager em;
+
+	@Autowired
+	DataSource dataSource;
+	private static final String baseUrl = "/api/v1/feedback";
+	@LocalServerPort
+	private int port;
+
+	private String authToken;
+
+	@BeforeAll
+	void setUpAll() {
+		try (Connection conn = dataSource.getConnection()) {
+			authToken = JwtTokenUtils.generateToken("fakeGoogleIdEmail", key, accessExpiredTimeMs);
+			ScriptUtils.executeSqlScript(conn, new ClassPathResource("/insert-user.sql"));
+			ScriptUtils.executeSqlScript(conn, new ClassPathResource("/insert-project.sql"));
+			ScriptUtils.executeSqlScript(conn, new ClassPathResource("/insert-team.sql"));
+			ScriptUtils.executeSqlScript(conn, new ClassPathResource("/insert-keyresult.sql"));
+			ScriptUtils.executeSqlScript(conn, new ClassPathResource("/insert-initiative.sql"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@BeforeEach
+	void setUp() {
+		RestAssured.port = port;
+	}
+
+	@Test
+	void 팀원의_행동전략에_피드백을_추가하면_기대하는_응답을_리턴한다() throws Exception {
+
+		final String response = RestAssured.
+
+			given()
+			.header("Authorization", "Bearer " + authToken)
+			.contentType(ContentType.JSON)
+			.body(new FeedBackSaveRequest("피드백 작성","GOOD_IDEA","project-fgFHxGWeIUQt","ini_ixYjj5nODqtb3AH8")).
+
+			when()
+			.post(baseUrl).
+
+			then()
+			.statusCode(HttpStatus.CREATED.value())
+			.extract().body().asString();
+
+		assertThat(response).containsPattern(
+			Pattern.compile("feedback-[a-zA-Z0-9]{11}"));
+
+	}
+}
