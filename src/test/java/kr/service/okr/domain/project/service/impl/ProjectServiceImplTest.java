421289ce14816f87a6caeab4be449bb01568d525
jaypark <pjhyun1990@gmail.com>
2023-03-25 15:49:56 +0900

    feat ProjectService::getRequiredFeedbackInitiative() 테스트 추가 & 구현 & 테스트 통과

--- src/test/java/kr/service/okr/domain/project/service/impl/ProjectServiceImplTest.java
+++ src/test/java/kr/service/okr/domain/project/service/impl/ProjectServiceImplTest.java
@@ -643,4 +643,12 @@ class ProjectServiceImplTest {
 		assertThat(singleProject.size()).isEqualTo(0L);
 	}
 
+	@Test
+	@Sql("classpath:insert-project-data.sql")
+	void getRequiredFeedbackInitiative을_호출하면_기대한는_응답_ProjectInitiativeResponse를_리턴한다() throws Exception {
+		final List<InitiativeInfo> response = sut.getRequiredFeedbackInitiative(3L);
+
+		assertThat(response.size()).isEqualTo(1);
+		assertThat(response.get(0).initiativeToken()).isEqualTo("ini_ixYjj5aaafeab3AH8");
+	}
 }
