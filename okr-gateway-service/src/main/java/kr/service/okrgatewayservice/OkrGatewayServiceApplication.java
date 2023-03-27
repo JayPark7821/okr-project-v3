e790ca70e47d480ff1192f0f00b7dfe9f65400e3
jaypark <pjhyun1990@gmail.com>
2023-03-27 22:41:22 +0900

    feat gateway service 추가

---
+++ okr-gateway-service/src/main/java/kr/service/okrgatewayservice/OkrGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package kr.service.okrgatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkrGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkrGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ okr-gateway-service/src/test/java/kr/service/okrgatewayservice/OkrGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.service.okrgatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkrGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
