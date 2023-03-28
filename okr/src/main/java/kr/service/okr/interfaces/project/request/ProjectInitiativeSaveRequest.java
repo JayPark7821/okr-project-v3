4b533e436183c314d4922d103bc56ef7c3044986
jaypark <pjhyun1990@gmail.com>
2023-03-28 22:28:58 +0900

    feat docker network bridge 설정 & gateway filter 추가

--- okr/src/main/java/kr/service/okr/interfaces/project/request/ProjectInitiativeSaveRequest.java
+++ okr/src/main/java/kr/service/okr/interfaces/project/request/ProjectInitiativeSaveRequest.java
@@ -9,7 +9,7 @@ public record ProjectInitiativeSaveRequest(
 	String keyResultToken,
 
 	@NotNull(message = "행동 전략을 작성해 주세요.")
-	@Size(max = 50, message = "행동 전략은 50자까지 작성할 수 있습니다.")
+	@Size(max = 20, message = "행동 전략은 20자까지 작성할 수 있습니다.")
 	String name,
 
 	@DateValid(message = "8자리의 yyyy-MM-dd 형식이어야 합니다.", pattern = "yyyy-MM-dd")
@@ -21,7 +21,7 @@ public record ProjectInitiativeSaveRequest(
 	String sdt,
 
 	@NotNull(message = "행동 전략의 상세 내용을 입력해 주세요.")
-	@Size(max = 200, message = "상세 내용은 200자까지 작성할 수 있습니다.")
+	@Size(max = 150, message = "상세 내용은 150자까지 작성할 수 있습니다.")
 	String detail
 
 ) {
