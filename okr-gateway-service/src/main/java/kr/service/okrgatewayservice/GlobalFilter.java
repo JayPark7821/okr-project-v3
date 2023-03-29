63532a6e253a8bcd670f288e7ca7bed4dbc91eba
jaypark <pjhyun1990@gmail.com>
2023-03-29 18:39:26 +0900

    feat gateway custom header 추가 & log 설정

--- okr-gateway-service/src/main/java/kr/service/okrgatewayservice/GlobalFilter.java
+++ okr-gateway-service/src/main/java/kr/service/okrgatewayservice/GlobalFilter.java
@@ -1,10 +1,13 @@
 package kr.service.okrgatewayservice;
 
+import java.util.UUID;
+
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -30,21 +33,34 @@ public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Conf
 
 	@Override
 	public GatewayFilter apply(Config config) {
-		// Custom Pre filter
-		return ((exchange, chain) -> {
+
+		return (exchange, chain) -> {
 			ServerHttpRequest request = exchange.getRequest();
 			ServerHttpResponse response = exchange.getResponse();
 
+			final String trxId = UUID.randomUUID().toString().substring(0, 8);
+
+			final ServerHttpRequest builtRequest = request.mutate()
+				.header("x-trx-log-id", trxId)
+				.build();
+
+			ServerWebExchange reBuiltExchange = exchange.mutate().request(builtRequest).build();
+
 			if (config.isPreLogger()) {
-				log.info("request id -> {}  Method: {} || RequestUrl: {} ", request.getId(), request.getMethod(),
-					request.getURI());
+				log.info("Request Started --> [TRX_ID] : {} , [IP] : {} , [REQUEST] : {}|{}",
+					trxId,
+					request.getRemoteAddress(),
+					request.getMethod(),
+					request.getURI()
+				);
 			}
-			//Custom Post Filter
-			return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+
+			return chain.filter(reBuiltExchange).then(Mono.fromRunnable(() -> {
 				if (config.isPostLogger()) {
 					log.info("Global Filter End: response code -> {} ", response.getStatusCode());
 				}
 			}));
-		});
+		};
+
 	}
 }
