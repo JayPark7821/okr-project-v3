d7188b0b5b322ae956254785e8f48d1015044e0c
jaypark <pjhyun1990@gmail.com>
2023-04-12 23:21:22 +0900

    feat 유저 도메인 서버 모듈 추가

---
+++ boot/okr-boot-user/src/main/java/kr/service/user/OkrBootUserApplication.java
@@ -0,0 +1,13 @@
+package kr.service.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkrBootUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkrBootUserApplication.class, args);
+	}
+
+}
