8ff006bee9234e45e1b20dd7d536f1938877ff7b
jaypark <pjhyun1990@gmail.com>
2023-04-15 23:22:20 +0900

    feat springcloud admin 추가

---
+++ cloud/okr-cloud-admin/src/main/java/kr/service/okr/OkrCloudAdminApplication.java
@@ -0,0 +1,13 @@
+package kr.service.okr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkrCloudAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkrCloudAdminApplication.class, args);
+	}
+
+}
