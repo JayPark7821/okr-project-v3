e312ea0f4815b704d2f41d6f0e60af2d9037d6fb
jaypark <pjhyun1990@gmail.com>
2023-04-23 00:07:24 +0900

    test ProjectApiControllerImpl::registerProject() 팀원추가하여 프로젝트 생성 실패하는 테스트 추가

--- boot/okr-boot-project/src/test/java/kr/service/okr/api/project/ProjectApiControllerImplTest.java
+++ boot/okr-boot-project/src/test/java/kr/service/okr/api/project/ProjectApiControllerImplTest.java
@@ -13,8 +13,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import kr.service.okr.common.security.core.context.AuthenticationInfo;
 import kr.service.okr.project.api.RegisterProjectRequestDto;
+import kr.service.okr.project.persistence.entity.project.team.TeamMemberJpaEntity;
 import kr.service.okr.user.enums.ProviderType;
 import kr.service.okr.user.user.domain.JobField;
 import kr.service.okr.user.user.domain.User;
@@ -30,6 +33,9 @@ class ProjectApiControllerImplTest extends SpringBootTestReady {
 		super.setUp();
 		dataLoader.loadData(List.of("/project-test-data.sql"));
 	}
+
+	@PersistenceContext
+	EntityManager em;
 
 	@Test
 	@DisplayName("팀원 없이 프로젝트를 생성하면 기대하는 응답(projectToken)을 반환한다.")
@@ -55,5 +61,38 @@ class ProjectApiControllerImplTest extends SpringBootTestReady {
 
 		assertThat(response.getBody()).containsPattern(
 			Pattern.compile("project-[a-zA-Z0-9]{12}"));
+	}
+
+	@Test
+	@DisplayName("프로젝트를 생성시 팀원을 같이 입력하면 기대하는 응답(projectToken)을 반환한다.")
+	void create_project_with_team_members() throws Exception {
+		String projectSdt = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+		String projectEdt = LocalDateTime.now().plusDays(10).format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+
+		final ResponseEntity<String> response = sut.registerProject(
+			new RegisterProjectRequestDto("projectObjective", projectSdt, projectEdt, List.of("guest@email.com")),
+			new AuthenticationInfo(
+				User.builder()
+					.userId("testId2")
+					.userSeq(2L)
+					.username("testUser2")
+					.email("projectMasterTest@naver.com")
+					.providerType(ProviderType.GOOGLE)
+					.jobField(JobField.PRODUCER_CP)
+					.profileImage("profile_image_url")
+					.build()
+			)
+		);
+
+		assertThat(response.getBody()).containsPattern(
+			Pattern.compile("project-[a-zA-Z0-9]{12}"));
+
+		final List<TeamMemberJpaEntity> teamMembers = em.createQuery(
+				"select t from TeamMemberJpaEntity t where t.project.projectToken = :projectToken",
+				TeamMemberJpaEntity.class)
+			.setParameter("projectToken", response.getBody())
+			.getResultList();
+		
+		assertThat(teamMembers.size()).isEqualTo(2);
 	}
 }
