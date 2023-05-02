4ac8cb143809add3cb6d0066fc1902b86b99110d
jaypark <pjhyun1990@gmail.com>
2023-05-02 22:50:48 +0900

    test 행동전략 추가시 진척도 업데이트 실패하는 테스트 추가

--- boot/okr-boot-project/src/test/java/kr/service/okr/acceptance/project/initiative/InitiativeAcceptanceTest.java
+++ boot/okr-boot-project/src/test/java/kr/service/okr/acceptance/project/initiative/InitiativeAcceptanceTest.java
@@ -40,6 +40,19 @@ public class InitiativeAcceptanceTest extends SpringBootTestReady {
 		행동전략_추가_요청_응답_검증(응답);
 	}
 
+	@Test
+	@DisplayName("행동전략 추가시 진척도가 변경된다 (동시성 테스트)")
+	void project_progress_will_change_when_initiative_added_concurrency_test() throws Exception {
+		//given
+		var 행동전략_생성_데이터 = 행동전략_저장_요청_데이터_생성("행동전략1", "key_wV6MX15WQ3DTzQMs", "행동전략 상세");
+
+		//when
+		행동전략_추가_요청_99건_동시성_테스트(행동전략_생성_데이터, 사용자_토큰);
+
+		//then
+		행동전략_추가_요청_동시성_테스트_검증(em);
+	}
+
 	private RegisterInitiativeRequest 행동전략_저장_요청_데이터_생성(String 행동전략명, String 핵심결과_토큰, String 행동전략_상세_내용) {
 		return new RegisterInitiativeRequest(
 			핵심결과_토큰,
